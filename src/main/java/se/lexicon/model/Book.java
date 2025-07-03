16bc13a5ed0e1696e30f691c12eb91fb62ba48fb
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-03 10:43:33 +0200

    Added getters and setters for Book and Person.

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -1,8 +1,7 @@
 package se.lexicon.model;
 
-
 /**
- * This class represents a Book model with properties and methods
+  * This class represents a Book model with properties and methods
  * to manage book-related information and operations.
  */
 public class Book {
@@ -13,9 +12,42 @@ public class Book {
     private String author;
     private boolean avaliable;
 
+    // Constructor:
+    public Book(String title, String author) {
+        this.id = id;
+        setTitle(title);
+        setAuthor(author);
+    }
+
     // Getters:
+    public String getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+
+        return this.title;
+    }
+
+    public String getAuthor() {
+
+        return this.author;
+    }
 
     // Setters:
+    public void setTitle(String title){
+        if (title == null || title.trim().isEmpty()){
+            throw new IllegalArgumentException("Title cannot be null or empty.");
+        }
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        if (author == null || author.trim().isEmpty()){
+            throw new IllegalArgumentException("Author cannot be null or empty.");
+        }
+        this.author = author;
+    }
 
     // Operations:
 
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -5,18 +5,54 @@ package se.lexicon.model;
  */
 public class Person {
 
-    private static int personIdCounter = 0;
+    private static int sequencer = 0;
 
     // Attributes:
     private int id;
     private String firstName;
     private String lastName;
 
+    // Constructor:
+    public Person(String firstName, String lastName) {
+        this.id = ++sequencer;
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
     // Getters:
+    public int getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+
+        return this.firstName;
+    }
+
+    public String getLastName() {
+
+        return this.lastName;
+    }
 
     // Setters:
+    public void setFirstName(String firstName){
+        if (firstName == null || firstName.trim().isEmpty()){
+            throw new IllegalArgumentException("Firstname cannot be null or empty.");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        if (lastName == null || lastName.trim().isEmpty()){
+            throw new IllegalArgumentException("Lastname cannot be null or empty.");
+        }
+        this.lastName = lastName;
+    }
 
     // Operations:
+    public String getPersonInformation() {
+        return this.firstName + " " + this.lastName;
+    }
 
 
 }
